dd56ba50634f53d514c4323a9b3f99ffa697879b
VishalG123 <vgupta@bhavnacorp.com>
2020-08-10 14:33:36 +0530

    test5

--- src/test/java/helper/DriverInitilization.java
+++ src/test/java/helper/DriverInitilization.java
@@ -52,8 +52,9 @@ public class DriverInitilization {
                 return WebDriverPool.DEFAULT.getDriver(new FirefoxOptions());
             case "chrome":
                 if (ServicePropertyFileReader.getInstance("env").getValue("os").equals("Windows")) {
-                    WebDriverManager.chromedriver().setup();
-                    return WebDriverPool.DEFAULT.getDriver(new ChromeOptions());
+                   // WebDriverManager.chromedriver().setup();
+                    System.setProperty("webdriver.chrome.driver", "D:/local_Domo/chromedriver.exe");
+                  //  return WebDriverPool.DEFAULT.getDriver(new ChromeOptions());
                 } else {
 
                     System.setProperty("webdriver.chrome.driver", ServicePropertyFileReader.getInstance("env").getPropertyValue("os"));
